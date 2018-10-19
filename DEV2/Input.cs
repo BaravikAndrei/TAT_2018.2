e5f802b52aaa84f699b5852305afb9be35b30be6
Andrei_Baravik <baravik.andrei@gmail.com>
2018-10-19 19:01:33 +0300

    DEV2

---
+++ DEV2/Input.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+
+namespace DEV2
+{
+   // Class for inputing string from comand line.
+   static class Input
+    {   
+        // Realisation of inputing string from comand line.  
+        public static string InputString(this string[] args)
+        {
+            string input="";
+            StringBuilder SumOfStrings = new StringBuilder();
+            //making a string from inputed strings from comaand line.
+            foreach (string i in args)
+            {
+                SumOfStrings.Append(i + " ");
+            }
+            input=(SumOfStrings.ToString());
+            if(input.Length>0)
+            { 
+                input.Remove(input.Length - 1, 1);  //Remove last space.
+            } 
+          
+            return input.ToLower();   //Return string at lower bracket
+        }
+    }
+}
---
+++ DEV2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace DEV2
+{
+    /*
+	    The main program class.
+	    Contains the entry point method of the programm.
+	*/
+    class TranslitString
+    {
+        // Entry point of the program
+	    // The entry point of the program, where the program control starts and ends.  
+	    // Get the value from the command line argument.
+       public static void Main(string[] args)
+        {    
+            // checking for empty input line
+            string input=args.InputString();
+            if (input.Length==0)
+            {
+                Console.WriteLine("You didn't input a string line");
+                Console.ReadKey();
+            }
+            else
+            { 
+                Console.WriteLine (input.Transliteration());
+                Console.ReadKey();
+            }
+        }
+    }
+      
+}
