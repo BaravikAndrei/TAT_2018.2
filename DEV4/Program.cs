2b4e559b2601bdc4fef0e853574aee3e179c7bdc
Andrei_Baravik <baravik.andrei@gmail.com>
2018-10-28 14:36:19 +0300

    Task_DEV4

---
+++ DEV4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DEV4
+{
+    class EnteryPoint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ParcerXML str = new ParcerXML(args[0]);
+                str.OutputtData();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("program error");
+            }
+        }
+    }
+}
