4c4b8fe4d829149ef8921fecff1bef0001edfc40
Andrei_Baravik <baravik.andrei@gmail.com>
2018-11-23 16:56:57 +0300

    Task_DEV7

---
+++ DEV7/DEV7/TransferRadix.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace DEV7
+{ 
+    /// <summary>
+    /// Class for transfer to another number system.
+    /// Contains the method of transfer from decimal to another number system.
+    /// </summary>
+    public class TransferRadix
+    {
+        /// <summary> 
+        /// Method TransferValueToAnotherNumberSystem.
+        /// Function transfer from decimal system to another selected system with radix from 2 to 20.
+        /// </summary> 
+        /// <param name="taked_value">Value in deciml system</param>
+        /// <param name="taked_number_of_system">Number of system to convert</param>
+        static string[] PossibleValuesOfTheSystem = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C",
+        "D", "E", "F", "G", "H", "I"};  //string of possible values of the radix 
+
+        public string TransferValueToAnotherNumberSystem(string valueInDecimalSystem, string newRadix)
+        {   
+            StringBuilder convertedValue=new StringBuilder();
+            //convert inputed strings into integer vaalues.
+            int inputedValue = Convert.ToInt32(valueInDecimalSystem); 
+            int numberOfRadix = Convert.ToInt32(newRadix);
+            //check for negative value 
+            string sign ="";
+            if (inputedValue < 0)
+            {
+                inputedValue = inputedValue * -1;
+                sign="-";
+            }
+            //transfer value from decimal to selected system.
+            int i = 0;
+            for (; ; )
+            {
+                if (inputedValue >= numberOfRadix)
+                {
+                    if (inputedValue % numberOfRadix == 0)
+                    {
+                        convertedValue.Insert (0, PossibleValuesOfTheSystem[0]);
+                        inputedValue /= numberOfRadix;
+                    }
+                    else if (inputedValue % numberOfRadix > 0)
+                    {
+                        convertedValue.Insert (0, PossibleValuesOfTheSystem[inputedValue % numberOfRadix]);
+                        inputedValue = inputedValue / numberOfRadix;
+                    }
+                }
+                else if(inputedValue < numberOfRadix)
+                {
+                    convertedValue.Insert (0, PossibleValuesOfTheSystem[inputedValue]);
+                    break;
+                }
+                i++;
+            }
+            convertedValue.Insert (0, sign);
+            return convertedValue.ToString(); //return converted value.
+        }
+    }
+}
---
+++ DEV7/Task_DEV3_Test/NUnitTestDEV3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using DEV7;
+
+namespace NUnitTestProject1
+{
+    [TestFixture]
+    public class NUnitTesTransferRadix
+    {
+        TransferRadix test = new TransferRadix();
+
+        [TestCase("-2147483647", "19", "-27C57H32")]
+        [TestCase("2147483647", "20", "1DB1F927")]
+        [TestCase("10", "2", "1010")]
+        [TestCase("-10", "11", "-A")]
+        [TestCase("0", "11", "0")]
+        [Test]
+        public void TestTransferValueToAnotherNumberSystem(string decimalValue, string radix, string result)
+        {
+            string tests = test.TransferValueToAnotherNumberSystem(decimalValue, radix);
+            Assert.AreEqual(tests, result);
+        }
+    }
+    [TestFixture]
+    public class NUnitTesCheckInput
+    {
+        CheckInput check = new CheckInput();
+        [TestCase("", "", false)]
+        [TestCase("3","", false)]
+        [TestCase("", "2", false)]
+        [TestCase("2147483647", "2", true)]
+        [TestCase("-2147483648", "11", true)]
+        [TestCase("543", "20", true)]
+        [TestCase("0", "11", true)]
+        [TestCase("255", "1", false)]
+        [TestCase("255", "-1", false)]
+        [TestCase("255", "21", false)]
+        [Test]
+        public void TestCheckForCorrectInput(string decimalValue, string radix, bool result)
+        {
+           
+            bool checks = check.CheckForCorrectInput(decimalValue, radix); 
+            Assert.AreEqual(checks, result);
+        }
+
+    }
+}
