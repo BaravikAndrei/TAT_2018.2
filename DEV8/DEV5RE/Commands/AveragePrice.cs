61aa2b8777333417e8685371a86b708e66913a8c
Andrei_Baravik <baravik.andrei@gmail.com>
2018-12-02 15:37:46 +0300

    Task_DEV8

---
+++ DEV8/DEV5RE/Commands/AveragePrice.cs
@@ -0,0 +1,27 @@
+﻿namespace DEV8.Commands
+{
+    /// <summary>
+    /// AveragePriceCommand implements method for command selection.
+    /// </summary>
+    class AveragePriceCommand : ICommand
+    {
+        Catalog catalog;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="catalog">Catalog of commnds.</param>
+        public AveragePriceCommand(Catalog catalog)
+        {
+            this.catalog = catalog;
+        }
+
+        /// <summary>
+        ///  Execute this instance.
+        /// </summary>
+        public string Execute(Catalog catalog)
+        {
+            return catalog.AveragePrice();
+        }
+    }
+}
---
+++ DEV8/DEV5RE/Invoker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using DEV8.Commands;
+
+namespace DEV8
+{
+    /// <summary>
+    /// Invoker used for invoking required methods.
+    /// </summary>
+    class Invoker
+    {
+        public List<ICommand> commands = new List<ICommand>();
+
+        /// <summary>
+        /// Initialize new Invoker object.
+        /// </summary>
+        /// <param name="countTypes">Command for count types.</param>
+        /// <param name="countCars">Command for count cars.</param>
+        /// <param name=averagePrice">Command for count average price of carrs.</param>
+        /// <param name="averagePriceOfTypes">Command for count average price of some type carrs.</param>
+        public Invoker(ICommand countTypes, ICommand countCars, ICommand averagePrice, ICommand averagePriceOfTypes)
+        {
+            commands.Add(countTypes);
+            commands.Add(countCars);
+            commands.Add(averagePrice);
+            commands.Add(averagePriceOfTypes);
+        }
+    }
+}
