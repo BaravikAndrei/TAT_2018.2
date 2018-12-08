28c974a6cd951059d13d47a98eea7ca5d87f9ee0
Andrei_Baravik <baravik.andrei@gmail.com>
2018-12-08 13:07:15 +0300

    KW_Task2

---
+++ KW2/CW2/IpSearchEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW2
+{
+    class IpSearchEngine
+    {
+        public string FinderIp(string args)
+        {
+
+            return
+        }
+    }
+}
---
+++ KW2/CW2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW2
+{
+    /// <summary>
+    /// Class EntryPoint is an entry point in the program.
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
