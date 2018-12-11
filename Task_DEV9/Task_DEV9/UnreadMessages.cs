215b1d40c20b2bf5f613ad484059612d04604465
Andrei_Baravik <baravik.andrei@gmail.com>
2018-12-11 16:08:06 +0300

    change cssselector on xpath

--- Task_DEV9/Task_DEV9/UnreadMessages.cs
+++ Task_DEV9/Task_DEV9/UnreadMessages.cs
@@ -21,12 +21,14 @@ namespace Task_DEV9
             options.AddArguments("--start-maximized", "--disable-notifications");
             IWebDriver browser = new ChromeDriver(options);
             browser.Navigate().GoToUrl(@"https://www.facebook.com/");
+
             browser.FindElement(By.Id("email")).SendKeys("+375293589083");
             browser.FindElement(By.Id("pass")).SendKeys("17505701");
             browser.FindElement(By.Id("loginbutton")).Click();
-            browser.FindElement(By.CssSelector("#u_0_a #u_0_d")).Click();
+            browser.FindElement(By.XPath("//*[@id='left_nav_section_nodes']/div/ul/li[2]")).Click();
             browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            var listMessages = browser.FindElements(By.CssSelector("div.uiScrollableAreaBody li a div div div._42ef div div.content div._1iji span span"));
+            var listMessages = browser.FindElements(By.XPath
+                ("//*[@id='globalContainer']/div/div/div/div/div/div[2]/div[3]/div/div/div/div/div[2]/div/ul/li/div/a/div/div[2]/div[2]/span/span"));
             foreach (IWebElement iw in listMessages)
             {
                 messages.Append(iw.Text + '\n');
